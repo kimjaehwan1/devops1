433de37d16670667494f6ea9d5cab0c6c5c605d2
kimjaehwan1 <a01088664190@gmail.com>
2024-06-01 17:53:25 +0900

    컨트롤러 수정

--- src/main/java/kr/ac/koreatech/devops1/HomeController.java
+++ src/main/java/kr/ac/koreatech/devops1/HomeController.java
@@ -1,5 +1,14 @@
 package kr.ac.koreatech.devops1;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+  @GetMapping("/")
+  public String getMethodName() {
+      return "index.html";
+  }
+  
 
 }
